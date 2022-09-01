315357c9a2eecedbdd88a5946f299fac7460df03
SNazarko <sorokinnazarne@gmail.com>
2022-09-01 13:40:31 +0300

    Initial Commit

---
+++ Shared/business_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  business_cardApp.swift
+//  Shared
+//
+//  Created by MacBook Forum on 01.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct business_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
